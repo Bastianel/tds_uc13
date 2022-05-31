d5211af91e91425e5b3e8fbf5f0af9afcc3cdb33
Bastianel <leobastianel@gmail.com>
2022-05-30 21:41:00 -0300

    Aula 30/05

--- myeats/controller/user_controller.php
+++ myeats/controller/user_controller.php
@@ -1,13 +1,18 @@
 <?php
+
     require_once "../model/user_model.php";
 
-    $nome = $_POST["nome"];
-    $endereco = $_POST["endereco"];
-    $email = $_POST["email"];
-    $senha = $_POST["senha"];
-    $conf_senha = $_POST["conf_senha"];
 
-    $userModel = new UserModel();// criar objeto    
-    $userModel -> create($nome, $endereco, $email, $senha);// chamar função
+
+    $name = $_POST['nome'];
+    $address = $_POST['endereco'];
+    $email = $_POST['email'];
+    $password = $_POST['senha'];
+    $confirm_password = $_POST['senha'];
+
+
+
+    $userModel = new UserModel(); //New é utilizado para instanciar objeto ;
+    $userModel -> create($name, $address, $email, $password); //Chama a funcao do UserModel com os parametros $name, $address, $email.....;
 
 ?>
--- myeats/model/user_model.php
+++ myeats/model/user_model.php
@@ -1,16 +1,36 @@
 <?php
-    class UserModel {
-            public function create($nome, $endereco, $email, $senha){
-                echo "create: ". $nome . "<br>". $endereco."<br>" .$email."<br>" .$senha."<br>" ;
-            } 
-            public function delete(){
-                echo "delete";
-            } 
-            public function update(){
-                echo "update";
-            }
-            public function findall(){
-                echo "findall";
-            }
+
+    require_once "../util/database.php";
+
+    class UserModel{
+
+        public function create($name, $address, $email, $password){
+            $db = new Database();
+            $con = $db->connect(); //-> faz com que o connect seja um objeto
+            $sql = "insert into user(name, email, address, password) values (:name, :email, :password,  :address)";
+            $stmt = $con -> prepare($sql);
+            $passwordcripto = sha-1($password);
+
+            $result = $stmt -> execute(["name" => $name, "email" => $email, "password" => $password, "address" => $address]);
+
+        }
+        public function delete(){
+            echo "delete";
+        }
+        public function update(){
+            echo "update";
+        }
+        public function findall(){
+            echo "find all";
+        }
+
+
+
+
     }
+
+
+
+
+
 ?>
